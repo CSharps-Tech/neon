5337a517fdb7d726cdefcffcefcb8d68761508b1
CSharps-Tech <114924195+CSharps-Tech@users.noreply.github.com>
2022-12-31 20:28:09 +0700

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,10 @@
+clear
+echo "Neon Bot\n Bot tạo máy chủ kho lưu trữ\n Bạn muốn tạo kho lưu trữ máy chủ dự án?\n\n Tùy chọn [Y/N]\n"
+read "Lựa chọn: " option
+if [$option -gt y]
+then
+fi
+if [$option -gt n]
+then
+echo "Bạn đã hủy thiết lập kho lưu trữ máy chủ"
+fi
