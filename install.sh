a81134dc9908bc93aafab46f74f694195eef6e79
CSharps-Tech <114924195+CSharps-Tech@users.noreply.github.com>
2023-01-02 08:36:36 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,11 +1,11 @@
 clear
 echo "Neon Bot\n Bot tạo máy chủ kho lưu trữ\n Bạn muốn tạo kho lưu trữ máy chủ dự án?\n\n Tùy chọn [Y/N]\n"
-read "Lựa chọn: " $option
-if [$OPTION -gt y]
+read "Lựa chọn: " option
+if [[$option == y]]
 then
 apt install python
 fi
-if [$OPTION -gt n]
+if [[$option == n]]
 then
 echo "Bạn đã hủy thiết lập kho lưu trữ máy chủ"
 fi
